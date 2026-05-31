3bcd6c28d8d98a2c189377e38ab2486cd370d1ad
thns <pandahealthcare@gmail.com>
2026-05-31 18:56:31 +1000

    Start repository

---
+++ README.md
@@ -0,0 +1,3 @@
+# Vybi
+
+[![Open in Bolt](https://bolt.new/static/open-in-bolt.svg)](https://bolt.new/~/sb1-7xsgb2nt)
---
+++ src/App.tsx
@@ -0,0 +1,7 @@
+import VybiApp from './vybi-app.jsx';
+
+function App() {
+  return <VybiApp />;
+}
+
+export default App;
